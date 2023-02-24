051acf25dda913a5b6aa8b4a94f4133b1bba557a
zhiye9 <fly9811@163.com>
2023-02-24 13:23:14 +0100

    load torchmetrics

--- N5K.sh
+++ N5K.sh
@@ -2,4 +2,5 @@
 module load tools
 module load pytorch/1.12.1 
 module load anaconda3/2020.07
+module load torchmetrics/0.11.1
 python3 /home/projects/cu_10108/data/Generated/ye_food_img/incept_v3/FoodImage_N5K/n5k_model.py -a swin_b --output_model './checkpoint_2_lr001_swin_b.pth' --output_val hist_5_val_lr1e-4_swin_b --output_train hist_5_train_lr1e-4_swin_b
