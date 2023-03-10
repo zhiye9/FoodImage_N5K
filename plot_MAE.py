b484250bd8235905cf08b27de9997e00fbabc044
zhiye9 <fly9811@163.com>
2023-03-10 22:57:23 +0100

    update

--- plot_MAE.py
+++ plot_MAE.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 PATH_train = "/home/zhi/data/FoodImage_N5K/results/hist_150_continue_150_dropout02_train_lr1e-4_swin_b"
 PATH_test = "/home/zhi/data/FoodImage_N5K/results/hist_150_continue_150_dropout02_val_lr1e-4_swin_b"
 
-PATH_train = "/home/zhi/data/FoodImage_N5K/results/hist_150_dropout_train_lr1e-4_inception_v3"
-PATH_test = "/home/zhi/data/FoodImage_N5K/results/hist_150_dropout_val_lr1e-4_inception_v3"
+PATH_train = "/home/zhi/data/FoodImage_N5K/results/hist_75_MAPE_train_lr1e-4_swin_b"
+PATH_test = "/home/zhi/data/FoodImage_N5K/results/hist_75_MAPE_val_lr1e-4_swin_b"
 
 with open(PATH_train, "rb") as fp:
     hist_50 = pickle.load(fp)
@@ -17,14 +17,14 @@ hist1 = []
 hist1 = [h for h in hist_50]
 hist2 = []
 hist2 = [h for h in hist_50_lr0001]
-num_epochs = 150
+num_epochs = 60
 
 import matplotlib as mpl
 mpl.rcParams['figure.dpi'] = 600
 
-plt.title("Swin-transformer with dropout")
+plt.title("Swin-transformer with log MAPE")
 plt.xlabel("Training Epochs")
-plt.ylabel("MAE")
+plt.ylabel("logMAPE")
 plt.plot(range(1,num_epochs+1),hist1,label="Training MAE (150 Epochs with lr 1e-4)")
 plt.plot(range(1,num_epochs+1),hist2,label="Validation MAE (150 Epochs with lr 1e-4)")
 #plt.yticks([])
